db2cb280a3764f9adbd5d7f6bbefa8ad3835ab63
Qwester471 <80134273+Qwester471@users.noreply.github.com>
2025-12-08 11:23:17 +0300

    Add files via upload

--- src/presentation/fastapi/routes/core/user_careers/api.py
+++ src/presentation/fastapi/routes/core/user_careers/api.py
@@ -4,6 +4,9 @@ from fastapi import APIRouter
 from fastapi import status
 from src.application.schemas.user_careers import CreateUserCareersSchema, UserCareersSchema
 from src.usecase.user_careers.create import CreateUserCareerUsecase
+from src.usecase.user_careers.schemas import GetUpdateUserCareersSchema
+from src.usecase.user_careers.update import UpdateUserCareersUsecase
+
 ROUTER = APIRouter(route_class=DishkaRoute)
 
 @ROUTER.post('', status_code=status.HTTP_200_OK)
@@ -11,3 +14,9 @@ async def create_users_career(
     usecase: FromDishka[CreateUserCareerUsecase],
     user: CreateUserCareersSchema) -> UserCareersSchema:
     return await usecase(user)
+
+@ROUTER.put('', status_code=status.HTTP_200_OK)
+async def update_user_careers(
+    usecase: FromDishka[UpdateUserCareersUsecase],
+    usercareer: GetUpdateUserCareersSchema) -> UserCareersSchema:
+    return await usecase(usercareer)
